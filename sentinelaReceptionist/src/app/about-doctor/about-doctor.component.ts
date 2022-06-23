83c290f0f2233e0046cb473cce0fae36b41067f4
Amanda-RVargas <amanda.jaques.vargas@outlook.com>
2022-06-22 21:58:53 -0300

    insertion of day and week day on calendar

--- sentinelaReceptionist/src/app/about-doctor/about-doctor.component.ts
+++ sentinelaReceptionist/src/app/about-doctor/about-doctor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const weekday = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+
 @Component({
   selector: 'app-about-doctor',
   templateUrl: './about-doctor.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutDoctorComponent implements OnInit {
 
-  daysQuantity: any;
-  counter: any={};
 
+  daysQuantity: any;
+  counter: any = {};
+  weekDay: any;
+  month: any;
+  year: any;
 
   constructor() { }
 
@@ -19,14 +24,23 @@ export class AboutDoctorComponent implements OnInit {
   }
 
   calcDays(): void {
-    var actualDate = new Date();   
-    var month  = actualDate.getMonth() + 1; 
-    var year = actualDate.getFullYear();
-    
-    this.daysQuantity = new Date(year, month, 0).getDate();  
+    var actualDate = new Date();
+    this.month = actualDate.getMonth() + 1;
+    this.year = actualDate.getFullYear();
+    this.daysQuantity = new Date(this.year, this.month, 0).getDate();
   }
 
   setCounterLength(): void {
     this.counter = new Array(this.daysQuantity);
+    for (var i = 1; i <= this.daysQuantity; i++) {
+      this.counter[i - 1] = i;
+    }
   }
+
+  getWeekDay(day: number): any {
+    var date = new Date(this.month + " " + day + ", " + this.year);
+    return weekday[date.getDay()];
+  }
+
+
 }
