f3c7b88d8a2f61f15f229fb547eb45fbf68ec193
Amanda-RVargas <amanda.jaques.vargas@outlook.com>
2022-06-23 14:44:22 -0300

    calendar upgrade

--- sentinelaReceptionist/src/app/about-doctor/about-doctor.component.ts
+++ sentinelaReceptionist/src/app/about-doctor/about-doctor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 const weekday = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+const month = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro"];
 
 @Component({
   selector: 'app-about-doctor',
@@ -14,6 +15,7 @@ export class AboutDoctorComponent implements OnInit {
   counter: any = {};
   weekDay: any;
   month: any;
+  monthName: any;
   year: any;
 
   constructor() { }
@@ -21,12 +23,25 @@ export class AboutDoctorComponent implements OnInit {
   ngOnInit(): void {
     this.calcDays();
     this.setCounterLength();
+  
+  }
+
+  checkToday(actualDay:number, actualMonth:number, actualYear:number) : boolean {
+    var checkDay = new Date().getDate();
+    var checkMonth = new Date().getMonth() + 1;
+    var checkYear = new Date().getFullYear();
+
+    if(checkDay == actualDay && checkMonth == actualMonth && checkYear == actualYear){
+    return true;
+    }
+    return false;
   }
 
   calcDays(): void {
     var actualDate = new Date();
     this.month = actualDate.getMonth() + 1;
     this.year = actualDate.getFullYear();
+    this.monthName = month[this.month - 1];
     this.daysQuantity = new Date(this.year, this.month, 0).getDate();
   }
 
@@ -40,6 +55,28 @@ export class AboutDoctorComponent implements OnInit {
   getWeekDay(day: number): any {
     var date = new Date(this.month + " " + day + ", " + this.year);
     return weekday[date.getDay()];
+  }
+
+  nextMonth(): void {
+    this.month++;
+    if(this.month == 13) {
+      this.month = 1;
+      this.year++;
+    }
+    this.monthName = month[this.month - 1];
+    this.daysQuantity = new Date(this.year, this.month, 0).getDate();
+    this.setCounterLength();
+  }
+
+  backMonth(): void {
+    this.month--;
+    if(this.month == 0) {
+      this.month = 12;
+      this.year--;
+    }
+    this.monthName = month[this.month - 1];
+    this.daysQuantity = new Date(this.year, this.month, 0).getDate();
+    this.setCounterLength();
   }
 
 
