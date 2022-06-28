6765c9a853f0604f37eaf97af7a045e4cb4acdc0
Amanda-RVargas <amanda.jaques.vargas@outlook.com>
2022-06-28 17:39:37 -0300

    patient schedule page done

--- sentinelaReceptionist/src/app/receptionist-panel/receptionist-panel.component.ts
+++ sentinelaReceptionist/src/app/receptionist-panel/receptionist-panel.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './receptionist-panel.component.html',
   styleUrls: ['./receptionist-panel.component.css']
 })
+
+
+
 export class ReceptionistPanelComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(event:Event): void {
+    const target = event.target as HTMLInputElement
+    const value = target.value
+
   }
 
 }
